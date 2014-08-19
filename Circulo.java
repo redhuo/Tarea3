a347986d42f638e0889291f181e252f7f72092e7
redhuo <jimmytsafg@gmail.com>
2014-08-19 16:53:36 -0600

    Errores
    ssa

--- Circulo.java
+++ Circulo.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,8 +8,8 @@
  *
  * @author Jimmy
  */
-public class Circulo{
-
+public class Circu
+no
     private double radio;
     private String color = "vacío";
     public static int cantidadCirculos =0;
